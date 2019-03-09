d741300cf303f76ae8d1e3e5064dd59930f347df
b0dybag <b0dybag@users.noreply.github.com>
2019-03-09 22:36:14 +0100

    added some styles

---
+++ my-app/src/app/server/server.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  // styleUrls: ['./server.component.css']
+  styles: [`
+    h3 {
+      color: brown;
+    }
+  `]
+})
+export class ServerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
