6f478923ee1f1e657b9f345f813076fcb721676b
b0dybag <b0dybag@users.noreply.github.com>
2019-03-25 23:08:06 +0100

    Added routers

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {Route, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { HeaderComponent } from './header/header.component';
 import { DropdownDirective } from './shared/dropdown.directive';
+
+const  appRouters: Route[] = [
+  {path: 'test3', component: 'test'}
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +21,8 @@ import { DropdownDirective } from './shared/dropdown.directive';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRouters)
   ],
   providers: [],
   bootstrap: [AppComponent]
